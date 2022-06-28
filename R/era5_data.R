a3ce3b95cad9f4577019f00187ec7884d4375f05
tliddell4 <tliddell@ucsb.edu>
2022-06-28 10:58:58 -0700

    Added documentation for example data, performed a check with 0 errors, 0 notes, and only 1 warning about licensing.

---
+++ R/era5_data.R
@@ -0,0 +1,9 @@
+#' Small sample of ERA5 Data
+#'
+#' One band of an ERA5 raster to set the default resolution and default
+#' coordinate reference system.
+#'
+#' @format A raster object containing data for one year, half a degree of
+#'   longitude, and 180.5 degrees of latitude.
+#'
+"demo_era5"
