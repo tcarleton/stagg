1b22e0c40ead3c2e6c6372e3d39f3c56bf57b206
tliddell4 <tliddell@ucsb.edu>
2022-07-07 11:47:42 -0700

    Set caresian = TRUE in merge so that function works in cases where nrow(dt_1[dt_2]) > nrow(dt_1) nrow(dt_2) and removed setMethod as it is unnecessary.

--- R/aggregate.R
+++ R/aggregate.R
@@ -12,6 +12,9 @@
 #' @param geoweights_table A table of weights which can be generated using
 #'   the function calc_geoweights()
 #' @param second_weights A boolean indicating whether secondary weights should be used
+#'
+#' @examples
+#' agg_climate_data(2011, demo_prcp, "prcp", "sum", "polynomial", 3, demo_output_geoweights, second_weights = TRUE)
 
 ## Sara Orofino
 ## February 22, 2022
@@ -38,7 +41,7 @@ agg_climate_data <- function(year, data_raster, climate_var, daily_agg, trans = 
     data.table::setnames(v, coln)
     v
   }
-  setMethod('as.data.table' signature(x='Raster'), as.data.table.raster)
+  #setMethod('as.data.table', signature(x='Raster'), as.data.table.raster)
 
 
 
@@ -84,7 +87,7 @@ agg_climate_data <- function(year, data_raster, climate_var, daily_agg, trans = 
     # Check if there are 24*365 or 24*366 layers
     if(!raster::nlayers(clim_raster) %in% c(8760, 8784)){
 
-      message(crayon::red("Warning: Incomplete year of data; raster has", length(all_layers),
+      warning(crayon::red("Warning: Incomplete year of data; raster has", length(all_layers),
                        "layers, but a complete year should have 8760 layers or 8784 layers on a leap year"))
     }
 
@@ -99,7 +102,7 @@ agg_climate_data <- function(year, data_raster, climate_var, daily_agg, trans = 
     # Check if there are 24*365 or 24*366 layers
     if(!raster::nlayers(clim_raster) %in% c(8760, 8784)){
 
-      message(crayon::red("Warning: Incomplete year of data; raster has", length(all_layers),
+      warning(crayon::red("Warning: Incomplete year of data; raster has", length(all_layers),
                        "layers, but a complete year should have 8760 layers or 8784 layers on a leap year"))
     }
 
@@ -125,7 +128,6 @@ agg_climate_data <- function(year, data_raster, climate_var, daily_agg, trans = 
     poly_orders <- seq(1:k) # Compute values from 1 to K
     list_length <- length(poly_orders) # How many lists are in the final object
     list_names <- sapply(1:list_length, FUN=function(x){paste("order", poly_orders[x], sep="_")})
-
     # For each daily layer, raise the value to k, k-1, k-2 etc. until 1
     r <- lapply(poly_orders, FUN=function(x){clim_daily ^ x})
 
@@ -166,8 +168,9 @@ agg_climate_data <- function(year, data_raster, climate_var, daily_agg, trans = 
   keycols = c("x", "y")
   data.table::setkeyv(clim_dt, keycols)
 
+
   # Keyed merge on the x/y column
-  merged_dt <- clim_dt[weights_dt] #cols: x, y, date, value cols 1:k, poly_id, w_area, weight (if weights = T)
+  merged_dt <- clim_dt[weights_dt, allow.cartesian = TRUE] #cols: x, y, date, value cols 1:k, poly_id, w_area, weight (if weights = T)
 
   ## Multiply weights x climate value (all 1:k values); aggregate by month and polygon
   ## -----------------------------------------------
@@ -184,7 +187,7 @@ agg_climate_data <- function(year, data_raster, climate_var, daily_agg, trans = 
   merged_dt[, ':=' (month = substring(date, first=1, last=8),
                     day = substring(date, first=10))]
 
-  # Can customize this in the future to aggregate by day & month
+  # Can customize this in the columnsfuture to aggregate by day & month
   # Right now just sum by month
   sum_by_poly <- merged_dt[,  lapply(.SD, sum), by = .(poly_id, month),
                            .SDcols = list_names]
@@ -192,7 +195,7 @@ agg_climate_data <- function(year, data_raster, climate_var, daily_agg, trans = 
   ## Add year column
   sum_by_poly[, year := year]
 
-  ## Order columns
+  ## Order
   data.table::setcolorder(sum_by_poly, neworder = c('year', 'month', 'poly_id', list_names))
 
   ## Return the sums by polygon
