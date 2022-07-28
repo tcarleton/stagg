747abc907b0fbd5d852d7ff13bb40128ac047a9d
tliddell4 <tliddell@ucsb.edu>
2022-07-28 09:47:59 -0700

    Changed era5_grid to 0s instead of near zeros, save 600 KB

--- R/data_era5_grid.R
+++ R/data_era5_grid.R
@@ -1,6 +1,6 @@
 #' Empty ERA5 grid.
 #'
-#' An empty raster layer with the spatial resolution of era5 data to be used in
+#' An empty raster layer with the spatial resolution of ERA5 data to be used in
 #' resampling a secondary raster.
 #'
 "era5_grid"
