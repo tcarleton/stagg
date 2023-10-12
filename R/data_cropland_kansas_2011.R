9d79f9b398113d913b244860da6f5217bb3b36a7
Sara Orofino <saraorofino@bren.ucsb.edu>
2023-10-12 13:26:01 -0700

    Remove the 2011 version of kansas cropland data; replace with 2015 version (data + R description file)

--- R/data_cropland_kansas_2011.R
+++
@@ -1,6 +0,0 @@
-#' Cropland data for a portion of Kansas.
-#'
-#' A high resolution raster layer depicting cropland over an area in Kansas 0.5
-#' degrees of latitude by 0.5 degrees of longitude.
-#'
-"cropland_kansas_2011"
