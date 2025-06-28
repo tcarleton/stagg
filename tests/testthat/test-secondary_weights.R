93aabb81dd45d10011769523a51e4fc99888a0ff
Tyler Liddell <ttliddell4@gmail.com>
2025-06-28 11:14:43 -0700

    Wrote passing tests for reprojecting secondary raster.

--- tests/testthat/test-secondary_weights.R
+++ tests/testthat/test-secondary_weights.R
@@ -67,19 +67,54 @@ test_that("secondary_weights errors", {
 test_that('secondary_weights works with mismatched projections', {
 
   # Create test data
-  test_secondary_raster <- terra::rast(cropland_nj_2015, crs = "+proj=laea +lat_1=52 +lon_0=-10 +ellps=GRS80") |>
-    terra::project("+proj=laea +lat_1=52 +lon_0=-10 +ellps=GRS80")
-  test_grid <- terra::rast(temp_nj_jun_2024_era5, crs = "ESRI:102761")[[1]] |>
-    terra::project("ESRI:102761")
+  test_secondary_raster <- terra::rast(cropland_nj_2015) |>
+    terra::project("ESRI:54008")
+  terra::crs(test_secondary_raster) <- "ESRI:54008"
 
   # Run secondary_weights on test data
-  test_output <- secondary_weights(test_secondary_raster, test_grid)
+  test_output <- secondary_weights(test_secondary_raster)
 
   # Get validation data
   validation_secondary_raster <- terra::rast(cropland_nj_2015)
-  validation_grid <- terra::rast(temp_nj_jun_2024_era5)[[1]]
-  validation_output <- secondary_weights(validation_secondary_raster, validation_grid)
+  validation_output <- secondary_weights(validation_secondary_raster, grid = era5_grid)
 
-  # Compare
-  expect_true(all.equal(test_output, validation_output))
+  # Join
+  joined_output <- dplyr::full_join(
+    dplyr::rename(test_output, test_weight = weight),
+    dplyr::rename(validation_output, validation_weight = weight),
+    by = c('x', 'y')
+  ) |>
+    dplyr::mutate(diff = test_weight - validation_weight) |>
+    dplyr::filter(!is.na(test_weight), !is.na(validation_weight))
+
+
+  # ggplot2::ggplot(joined_output) +
+  #   ggplot2::geom_tile(ggplot2::aes(x,y, fill = test_weight))
+  #
+  # ggplot2::ggplot(joined_output) +
+  #   ggplot2::geom_tile(ggplot2::aes(x,y, fill = validation_weight))
+  #
+  # ggplot2::ggplot(joined_output) +
+  #   ggplot2::geom_histogram(ggplot2::aes(x = diff))
+  #
+  # ggplot2::ggplot(joined_output) +
+  #   ggplot2::geom_point(ggplot2::aes(x = validation_weight, y = test_weight)) +
+  #   ggplot2::scale_color_viridis_c()
+
+
+
+  # Compute regression and make sure coefficients are within reason
+  reg <- lm(test_weight ~ 0 + validation_weight + x + y, joined_output)
+
+  # Location should play little role
+  expect_lt(reg$coefficients[['x']], 0.1)
+  expect_lt(reg$coefficients[['y']], 0.1)
+
+  # validation_weight should be near 1
+  expect_gt(reg$coefficients[['validation_weight']], 0.9)
+  expect_lt(reg$coefficients[['validation_weight']], 1.1)
+
+  # Should be fairly strong correlation
+  r_squared <- cor(joined_output$validation_weight, joined_output$test_weight)^2
+  expect_gt(r_squared, 0.8)
 })
